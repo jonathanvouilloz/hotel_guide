6d5a67acfd22a7547fc22e6acaa3403249c1d68f
Jonathan <jonathan.vouilloz@gmail.com>
2026-01-15 12:35:28 +0700

    add tinalock

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '8c5279d3ed6a87638d45ed378bd82130ef6beb86', queries,  });
 export default client;
   
